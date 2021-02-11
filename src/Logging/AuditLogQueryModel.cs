748a34cbcf980d565ab1eaa42aca7ff1b9311ff6
Rob Kennedy <rkennedy@vasont.com>
2021-02-11 00:06:16 -0500

    Pipeline Run
    Added Pipeline run models.

--- src/Logging/AuditLogQueryModel.cs
+++ src/Logging/AuditLogQueryModel.cs
@@ -6,6 +6,7 @@
 namespace Vasont.Publisher.Models.Logging
 {
     using System;
+    using System.Collections.Generic;
     using Vasont.Publisher.Models.Queries;
 
     /// <summary>
@@ -17,6 +18,18 @@ namespace Vasont.Publisher.Models.Logging
         /// Gets or sets the search text of the paged query.
         /// </summary>
         public string SearchText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the events.
+        /// </summary>
+        /// <value>The events.</value>
+        public List<AuditEvent> Events { get; set; } = new List<AuditEvent>();
+
+        /// <summary>
+        /// Gets or sets the results.
+        /// </summary>
+        /// <value>The results.</value>
+        public List<AuditResult> Results { get; set; } = new List<AuditResult>();
 
         /// <summary>
         /// Gets or sets the start date of the paged query.
---
+++ src/Pipeline/PipelineRunQueryRequestModel.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PipelineRunQueryRequestModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Publisher.Models.Pipeline
+{
+    using System.Collections.Generic;
+    using Vasont.Publisher.Models.Queries;
+
+    /// <summary>
+    /// This class contains query request parameters for browsing pipeline configurations.
+    /// </summary>
+    /// <seealso cref="Vasont.Publisher.Models.Pipeline.PipelineQueryRequestModel" />
+    public class PipelineRunQueryRequestModel : PaginatedQueryRequestModel
+    {
+        /// <summary>
+        /// Gets or sets the states.
+        /// </summary>
+        /// <value>The states.</value>
+        public List<PipelineRunState> States { get; set; } = new List<PipelineRunState>();
+
+        /// <summary>
+        /// Gets or sets the search text.
+        /// </summary>
+        /// <value>The search text.</value>
+        public string SearchText { get; set; }
+    }
+}

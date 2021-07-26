783da2c9efd693c38d4823fe5abe2dbb8a9cc66b
Rob Kennedy <rob@talegen.com>
2021-07-26 15:04:02 -0400

    Updated some minor tweaks for role models.

---
+++ src/Security/BrowseRoleModel.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BrowseRoleModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Publisher.Models.Security
+{
+    using System;
+
+    /// <summary>
+    /// This class represents a role in the browse API endpoint.
+    /// </summary>
+    /// <seealso cref="Vasont.Publisher.Models.Security.MinimalRoleModel" />
+    public class BrowseRoleModel : MinimalRoleModel
+    {
+        /// <summary>
+        /// Gets or sets the record created date time.
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the record last updated date time.
+        /// </summary>
+        public DateTime UpdatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user count.
+        /// </summary>
+        /// <value>The user count.</value>
+        public int UserCount { get; set; }
+    }
+}
